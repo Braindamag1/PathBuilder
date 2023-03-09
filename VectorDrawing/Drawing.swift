788a3389675d113caf6a5625b9659300081cd566
YJ.Li <yuanjun.li@beyond-power.com>
2023-03-09 13:34:13 +0800

    feat: live updates curve

--- VectorDrawing/Drawing.swift
+++ VectorDrawing/Drawing.swift
@@ -9,22 +9,28 @@ import SwiftUI
 
 struct Drawing: View {
     @State var path: Path = .init()
+    @GestureState var currentDrag: DragGesture.Value? = nil
+    var livePath: Path {
+            var copy = path
+            if let state = currentDrag {
+                copy.update(for: state)
+            }
+            return copy
+        }
     var body: some View {
         ZStack(alignment: .topLeading) {
             Color.white
-            path.stroke(Color.black, lineWidth: 2)
-            Points(path: path)
-                .background(Color.red)
+            livePath.stroke(Color.black, lineWidth: 2)
+            Points(path: livePath)
+
         }.gesture(
             // local means entire window
             DragGesture(minimumDistance: 0, coordinateSpace: .local)
+                .updating($currentDrag, body: { value, state, _ in
+                    state = value
+                })
                 .onEnded({ state in
-                    if path.isEmpty {
-                        path.move(to: state.startLocation)
-                    } else {
-                        path.addLine(to: state.startLocation)
-                    }
-
+                    path.update(for: state)
                 })
         )
     }
@@ -36,27 +42,77 @@ extension Path {
         forEach({ results.append($0) })
         return results
     }
+
+    mutating func update(for state: DragGesture.Value) {
+        if isEmpty {
+            move(to: state.startLocation)
+        } else {
+            let isDrag = state.startLocation.distance(to: state.location) > 1
+            if isDrag {
+                let control = state.location.mirrored(relativeTo: state.startLocation)
+                addQuadCurve(to: state.startLocation, control: control)
+            } else {
+                addLine(to: state.startLocation)
+            }
+        }
+    }
 }
 
 extension Path.Element: Identifiable { // hack for ForEach
     public var id: String { "\(self)" }
 }
 
+extension CGPoint {
+    func distance(to point: CGPoint) -> CGFloat {
+        let dx = (point.x - x) * (point.x - x)
+        let dy = (point.y - y) * (point.y - y)
+        return sqrt(dx + dy)
+    }
+
+    func mirrored(relativeTo p: CGPoint) -> CGPoint {
+        let relative: CGPoint = .init(x: x - p.x, y: y - p.y)
+        return .init(x: p.x - relative.x, y: p.y - relative.y)
+    }
+}
+
 struct PathPoint: View {
     var element: Path.Element
+    func pathPoint(at point: CGPoint) -> some View {
+        Circle()
+            .stroke(Color.black)
+            .background(
+                Circle()
+                    .fill(Color.white)
+            )
+            .padding(2)
+            .frame(width: 14, height: 14)
+            .offset(x: point.x - 7, y: point.y - 7)
+    }
+
+    func controlPoint(at point: CGPoint) -> some View {
+        RoundedRectangle(cornerRadius: 2)
+            .stroke(Color.black)
+            .background(RoundedRectangle(cornerRadius: 2).fill(Color.white))
+            .padding(4)
+            .frame(width: 14, height: 14)
+            .offset(x: point.x - 7, y: point.y - 7)
+    }
+
     var body: some View {
         switch element {
         case let .line(to: point),
              let .move(to: point):
-            Circle()
-                .stroke(Color.black)
-                .background(
-                    Circle()
-                        .fill(Color.white)
-                )
-                .padding(2)
-                .frame(width: 14, height: 14)
-                .offset(x: point.x - 7, y: point.y - 7)
+            pathPoint(at: point)
+        case let .quadCurve(to: to, control: control):
+            let mirrored = control.mirrored(relativeTo: to)
+            Path { path in
+                path.move(to: control)
+                path.addLine(to: to)
+                path.addLine(to: mirrored)
+            }.stroke(Color.gray)
+            pathPoint(at: to)
+            controlPoint(at: control)
+            controlPoint(at: mirrored)
         default:
             EmptyView()
         }
